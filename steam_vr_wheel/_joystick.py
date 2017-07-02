c3cb9029190edd32785517852d3886c6f1840a0c
mdovgialo <marian.dowgialo@gmail.com>
2017-07-02 14:35:03 +0200

    Changed the way grabbable joystick operates.
    Now it will be relative joystick - remembers the state when you let go
    of it, and when you grab it again it will change relatively to last
    remembered position, so there will be no hard jerks of planes on regrab.

--- steam_vr_wheel/_joystick.py
+++ steam_vr_wheel/_joystick.py
@@ -7,8 +7,8 @@ from steam_vr_wheel._virtualpad import VirtualPad, RightTrackpadAxisDisablerMixi
 
 class Throttle():
 
-    def __init__(self, size=0.3, inverted = False):
-        self.throttle_z = 0
+    def __init__(self, size=0.3, inverted = False, starting = 0):
+        self.throttle_z = starting
         self.throttle_real_world_z = 0
         self.throttle_at_real_world_z = 0
         self.throttle_relative_zeroed = False
@@ -50,7 +50,31 @@ class Joystick(RightTrackpadAxisDisablerMixin, LeftTrackpadAxisDisablerMixin, Vi
         self.throttle_z = Throttle(size=0.3, inverted=True)
         self.throttle_y = Throttle(size=0.2)
         self.throttle_x = Throttle(size=90)
+
+        self.grabbable_x = Throttle(size=1, starting=0.5)
+        self.grabbable_y = Throttle(size=1, starting=0.5)
+        self.grabbable_z = Throttle(size=1, starting=0.5)
+
         self.joystick_grabbed = False
+
+    @property
+    def joystick_grabbed(self):
+        try:
+            return self._joystick_grabbed
+        except:
+            return False
+
+    @joystick_grabbed.setter
+    def joystick_grabbed(self, x):
+        self._joystick_grabbed = bool(x)
+        if x:
+            self.grabbable_x.grabbed()
+            self.grabbable_y.grabbed()
+            self.grabbable_z.grabbed()
+        else:
+            self.grabbable_x.ungrabbed()
+            self.grabbable_y.ungrabbed()
+            self.grabbable_z.ungrabbed()
 
     def set_button_press(self, button, hand):
         super().set_button_press(button, hand)
@@ -73,21 +97,37 @@ class Joystick(RightTrackpadAxisDisablerMixin, LeftTrackpadAxisDisablerMixin, Vi
         if button == openvr.k_EButton_Grip and hand == 'right' and (not self.config.joystick_grabbing_switch):
             self.joystick_grabbed = False
 
+    def _update_joystick_normal(self, axisX, axisY, axisZ):
+
+        self.device.set_axis(HID_USAGE_X, int(axisX * 0x8000))
+        self.device.set_axis(HID_USAGE_Y, int(axisY * 0x8000))
+        self.device.set_axis(HID_USAGE_Z, int(axisZ * 0x8000))
+
+    def _update_grabbable_joystick(self, axisX, axisY, axisZ):
+
+        self.grabbable_x.update(axisX)
+        self.grabbable_y.update(axisY)
+        self.grabbable_z.update(axisZ)
+
+        self.device.set_axis(HID_USAGE_X, int(self.grabbable_x.x * 0x8000))
+        self.device.set_axis(HID_USAGE_Y, int(self.grabbable_y.x * 0x8000))
+        self.device.set_axis(HID_USAGE_Z, int(self.grabbable_z.x * 0x8000))
 
     def update(self, left_ctr, right_ctr):
         super().update(left_ctr, right_ctr)
-        axisX = (-right_ctr.yaw + 90)/180
-        if right_ctr.roll>=0:
+
+        axisX = (-right_ctr.yaw + 90) / 180
+        if right_ctr.roll >= 0:
             axisY = 90 - right_ctr.roll
         else:
             axisY = -(90 + right_ctr.roll)
-        axisY = -(axisY + 90)/180 + 1
-        axisZ = (-right_ctr.pitch+90)/180
+        axisY = -(axisY + 90) / 180 + 1
+        axisZ = (-right_ctr.pitch + 90) / 180
 
-        if (not self.config.joystick_updates_only_when_grabbed) or self.joystick_grabbed:
-            self.device.set_axis(HID_USAGE_X, int(axisX * 0x8000))
-            self.device.set_axis(HID_USAGE_Y, int(axisY * 0x8000))
-            self.device.set_axis(HID_USAGE_Z, int(axisZ * 0x8000))
+        if self.config.joystick_updates_only_when_grabbed:
+            self._update_grabbable_joystick(axisX, axisY, axisZ)
+        else:
+            self._update_joystick_normal(axisX, axisY, axisZ)
 
         self.throttle_z.update(left_ctr.z)
         self.throttle_y.update(left_ctr.y)
